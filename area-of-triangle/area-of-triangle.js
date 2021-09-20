e60897c47eafbe145b2f1c5e395a8672073388af
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-09-20 20:36:06 +0530

    calculated semi-perimeter and area

--- area-of-triangle/area-of-triangle.js
+++ area-of-triangle/area-of-triangle.js
@@ -5,6 +5,20 @@ var calculateAreaButton = document.querySelector("#btn-area");
 var outputText = document.querySelector("#output-text");
 
 function calculateArea(){
+    var firstSide = Number(firstSideOfTriangle.value);
+    var secondSide = Number(secondSideOfTriangle.value);
+    var thirdSide = Number(thirdSideOfTriangle.value);
+    var semiPerimeter = Number((firstSide + secondSide + thirdSide)/2);
+    console.log(firstSide,secondSide,thirdSide);
+    console.log(semiPerimeter);
+    //(firstSide + secondSide + thirdSide) = perimeter of the triangle
+    var area = Math.sqrt(semiPerimeter*(semiPerimeter - firstSide)*(semiPerimeter - secondSide)*(semiPerimeter - thirdSide));
+    console.log(area);
+
+
+    
+    
+
     
 }
 
