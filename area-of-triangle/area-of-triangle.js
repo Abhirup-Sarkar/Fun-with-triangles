fa8b963b5869949a8eb5df5dc5f6ce247f00b7e0
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-09-20 20:34:56 +0530

    fixed third side input

--- area-of-triangle/area-of-triangle.js
+++ area-of-triangle/area-of-triangle.js
@@ -1,6 +1,6 @@
 var firstSideOfTriangle = document.querySelector("#first-side");
 var secondSideOfTriangle = document.querySelector("#second-side");
-var thirdSideOfTriangle = document.querySelector("third-side");
+var thirdSideOfTriangle = document.querySelector("#third-side");
 var calculateAreaButton = document.querySelector("#btn-area");
 var outputText = document.querySelector("#output-text");
 
