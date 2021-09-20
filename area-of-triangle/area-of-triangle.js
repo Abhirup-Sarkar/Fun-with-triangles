ce54d4b01bfcdfb7c41915186ef158773c4cc674
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-09-20 20:51:19 +0530

    removed console.log, beautify file, JS done

--- area-of-triangle/area-of-triangle.js
+++ area-of-triangle/area-of-triangle.js
@@ -4,24 +4,15 @@ var thirdSideOfTriangle = document.querySelector("#third-side");
 var calculateAreaButton = document.querySelector("#btn-area");
 var outputText = document.querySelector("#output-text");
 
-function calculateArea(){
+function calculateArea() {
     var firstSide = Number(firstSideOfTriangle.value);
     var secondSide = Number(secondSideOfTriangle.value);
     var thirdSide = Number(thirdSideOfTriangle.value);
-    var semiPerimeter = Number((firstSide + secondSide + thirdSide)/2);
-    console.log(firstSide,secondSide,thirdSide);
-    console.log(semiPerimeter);
-    //(firstSide + secondSide + thirdSide) = perimeter of the triangle
-    var area = Math.sqrt(semiPerimeter*(semiPerimeter - firstSide)*(semiPerimeter - secondSide)*(semiPerimeter - thirdSide));
+    var semiPerimeter = Number((firstSide + secondSide + thirdSide) / 2); //(firstSide + secondSide + thirdSide) = perimeter of the triangle
+    var area = Math.sqrt(semiPerimeter * (semiPerimeter - firstSide) * (semiPerimeter - secondSide) * (semiPerimeter - thirdSide));
     var roundOffArea = area.toFixed(4);
-    console.log(roundOffArea);
-    outputText.innerHTML = "Area of the triangle using Heron's formula is "+roundOffArea+" units"
+    outputText.innerHTML = "Area of the triangle using Heron's formula is " + roundOffArea + " units";
 
-
-    
-    
-
-    
 }
 
 calculateAreaButton.addEventListener("click", calculateArea);
