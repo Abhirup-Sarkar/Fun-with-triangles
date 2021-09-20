7e41429615fa2da65e168dbf7a4fc4d6dcbd27e9
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-09-20 22:52:38 +0530

    select input tags, button and output-text using querySelector

---
+++ hypotenuse/hypotenuse.js
@@ -0,0 +1,9 @@
+var baseOfTriangle = document.querySelector("#base");
+var heightOfTriangle = document.querySelector("#height");
+var calculateHypotenuseButton = document.querySelector("#btnCalculateHypotenuse");
+var outputText = document.querySelector("#output-text");
+
+
+
+
+
