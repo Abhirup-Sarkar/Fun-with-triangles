c63543dfc3f5c64b2d77f798e236f07724d4f0be
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-09-20 22:57:15 +0530

    added EventListener and added function

--- hypotenuse/hypotenuse.js
+++ hypotenuse/hypotenuse.js
@@ -1,8 +1,13 @@
 var baseOfTriangle = document.querySelector("#base");
 var heightOfTriangle = document.querySelector("#height");
-var calculateHypotenuseButton = document.querySelector("#btnCalculateHypotenuse");
+var calHypotenuseButton = document.querySelector("#btnCalculateHypotenuse");
 var outputText = document.querySelector("#output-text");
 
+function calculateHypotenuse(){
+    
+}
+
+calHypotenuseButton.addEventListener("click", calculateHypotenuse);
 
 
 
