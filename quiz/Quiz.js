3bed19df7cd4cd9f04249a7930ad00a5afee7eaa
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-09-21 22:30:23 +0530

    added eventlistener and added function

--- quiz/Quiz.js
+++ quiz/Quiz.js
@@ -1,1 +1,15 @@
+var answersOne = document.querySelectorAll("#answer-1");
+var answersTwo = document.querySelectorAll("#answers-2");
+var answersThree = document.querySelectorAll("#answers-3");
+
+var btnSubmitAnswers = document.querySelector("#submit-answers");
+
+var outputText = document.querySelector("#output-text");
+
+function calculateScore(){
+    console.log("clicked");
+}
+
+btnSubmitAnswers.addEventListener("click", calculateScore )
+
 
