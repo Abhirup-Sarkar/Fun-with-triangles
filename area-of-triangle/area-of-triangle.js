539794d217374a74780193c49dd584a04ffe1176
Abhirup Sarkar <76419771+Abhirup-Sarkar@users.noreply.github.com>
2021-09-25 17:03:33 +0530

    removed roundoff and changed css and js file name

--- area-of-triangle/area-of-triangle.js
+++ area-of-triangle/area-of-triangle.js
@@ -10,8 +10,8 @@ function calculateArea() {
     var thirdSide = Number(thirdSideOfTriangle.value);
     var semiPerimeter = Number((firstSide + secondSide + thirdSide) / 2); //(firstSide + secondSide + thirdSide) = perimeter of the triangle
     var area = Math.sqrt(semiPerimeter * (semiPerimeter - firstSide) * (semiPerimeter - secondSide) * (semiPerimeter - thirdSide));
-    var roundOffArea = area.toFixed(4);
-    outputText.innerHTML = "Area of the triangle using Heron's formula is " + roundOffArea + " units";
+    
+    outputText.innerHTML = "Area of the triangle using Heron's formula is " + area + " units";
 
 }
 
